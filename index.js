87492b2d26a43b1d59f3df99a943c9af93f55041
AldrinKP <aldrinpastores@gmail.com>
2023-11-16 16:58:56 +0100

    Styled input and button. Created eventListener for button

---
+++ index.js
@@ -0,0 +1,7 @@
+let myLeads = [];
+const inputButton = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+
+inputButton.addEventListener('click', () => {
+	myLeads.push('www.awesomelead.com');
+});
